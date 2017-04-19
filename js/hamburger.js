25160b838f2b34529b77974e948050decc515996
Amos <amosgathungu@gmail.com>
2017-04-19 16:59:50 -0400

    overlay added on projects

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,17 @@
-$(document).ready(function(){
-    $('.hamburger').on('click', function(){
-        //$('.menu').addClass("open");
-        $('.menu').toggleClass("display");
-    });
-})
+// $(document).ready(function(){
+//     $('.hamburger').on('click', function(){
+//         //$('.menu').addClass("open");
+//         $('.menu').toggleClass("display");
+//     });
+// })
+
+$('.hamburger').on('click', function(){
+
+    if($('.menu').hasClass('.open')) {
+        $('.menu').removeClass('.open');
+    } else {
+        $('.menu').addClass('.open');
+    }
+
+    $('.menu').toggleClass('.open');
+});
