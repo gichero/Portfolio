8564f31a4e9e6872e7a0d3d081efcd9189b65b92
Amos <amosgathungu@gmail.com>
2017-04-26 16:55:47 -0400

    hamburger menu fixed

--- js/hamburger.js
+++ js/hamburger.js
@@ -7,11 +7,12 @@
 
 $('.hamburger').on('click', function(){
 
-    if($('.menu').hasClass('.open')) {
-        $('.menu').removeClass('.open');
-    } else {
-        $('.menu').addClass('.open');
-    }
+    // if($('.menu').hasClass('.open')) {
+    //     $('.menu').removeClass('.open');
+    // } else {
+    //     $('.menu').addClass('.open');
+    // }
 
-    $('.menu').toggleClass('.open');
+    $('.menu').toggleClass('open');
+
 });
